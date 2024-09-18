885d3dc15cd74e36224f9ac9434159cade543ff1
233haeun <233haeun@gmail.com>
2024-09-18 15:41:22 +0900

    ch1_3

---
+++ ch1_3.py
@@ -0,0 +1,36 @@
+import turtle
+
+turtle.Screen()
+turtle.setup(1000,1000)
+
+t = turtle.Turtle()
+
+t.penup()
+t.backward(200)
+t.pendown()
+
+t.circle(100)
+
+t.penup()
+t.forward(230)
+t.pendown()
+
+t.circle(100)
+
+t.penup()
+t.forward(230)
+t.pendown()
+
+t.circle(100)
+
+t.penup()
+t.goto(140,-130)
+t.pendown()
+
+t.circle(100)
+
+t.penup()
+t.backward(230)
+t.pendown()
+
+t.circle(100)
